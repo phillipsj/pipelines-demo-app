8800b85918d703cdd8472fdb28e069469240d436
Jamie Phillips <cphill11@gmail.com>
2018-11-02 21:29:06 -0400

    Basic app.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pipelines_demo_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the pipelines demo app!");
+        }
+    }
+}
